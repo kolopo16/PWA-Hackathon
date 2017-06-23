c039c40d57a96c724214d352905ac393ab7d7f91
Gead <ke.adkead@hotmail.com>
2017-06-24 02:03:20 +0700

    Hamburger menu

---
+++ src/components/HamburgerMenu.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HamburgerMenu extends Component {
+  render() {
+    return (
+      <label className="hamburger-menu" for="hamburger-menu">
+        <span></span>
+      </label>
+    );
+  }
+}
+
+export default HamburgerMenu;
