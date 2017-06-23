292fcc996a7f3caee122cc8284a1aed5f5fba79d
Koolapong Kongpitak <unlimited.g001@gmail.com>
2017-06-23 22:37:49 +0700

    fix icons not found

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+
+export default function firebaseConfig() {
+
+    const config = {
+        apiKey: "AIzaSyAUU-iLJn8goMSaR31lDW3DKXsoMrG3bxM",
+        authDomain: "pwa-hackathon-4042d.firebaseapp.com",
+        databaseURL: "https://pwa-hackathon-4042d.firebaseio.com",
+        projectId: "pwa-hackathon-4042d",
+        storageBucket: "pwa-hackathon-4042d.appspot.com",
+        messagingSenderId: "1035691895398"
+    };
+
+    firebase.initializeApp(config);
+
+    return firebase;
+};
