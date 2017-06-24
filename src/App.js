f23de6b0592d928e85e20bc3ccba69539864f820
Koolapong Kongpitak <unlimited.g001@gmail.com>
2017-06-24 07:46:03 +0700

    Add google api service

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 import './style/main.css';
 import BackToTopButton from './components/BackToTopButton';
+import googleServices from './googleConfig';
 
 class App extends Component {
+
+  componentDidMount() {
+
+    const services = new googleServices().service;
+    console.log(services,'app');
+    
+    // service.textSearch({
+    //   query: 'อุทยานแห่งชาติ'
+    // }, function (places, status) {
+    //   console.log('Place search:', places);
+    // });
+
+  }
+
+  googleServiceFunction() {
+    console.log(this.service, 'google');
+  }
+
   render() {
+    
+    
     return (
       <div>
         <h1>Hello World!</h1>
